leahneukirchen/extrace

commit c0e8a5424dfc2383eb2c996bd3e565c4aa3d59da

extrace: skip parent lookup for pid 1
I was getting a lot of errors that looked like

    extrace: process vanished before we found its parent: pid 1: redirfd

Which are easily avoidable.
Message-Id: <20210615120801.121874-1-hi@alyssa.is>

extrace.c
@@ -140,6 +140,9 @@ pid_depth(pid_t pid)
 	char *s;
 	int fd, d, i;
 
+	if (pid == 1)
+		return 0;
+
 	snprintf(name, sizeof name, "/proc/%d/stat", pid);
 
 	if ((fd = open(name, O_RDONLY)) < 0)

commit 06a771fcfb511d2fab024d5970c9f4495abab8c3

fix -p flag
Regression from c0e8a5424d, also fix error reporting of processes
we don't care about.

extrace.c
@@ -140,7 +140,7 @@ pid_depth(pid_t pid)
 	char *s;
 	int fd, d, i;
 
-	if (pid == 1)
+	if (pid == 1 && parent == 1)
 		return 0;
 
 	snprintf(name, sizeof name, "/proc/%d/stat", pid);
@@ -345,14 +345,16 @@ handle_msg(struct cn_msg *cn_hdr)
 
 		d = pid_depth(pid);
 		if (d < 0) {
-			if (*cmdline) {
-				print_runtime_error(
-				    "extrace: process vanished before we found its parent: pid %d: %s\n",
-				    pid, cmdline);
-			} else {
-				print_runtime_error(
-				    "extrace: process vanished without a name: pid %d\n",
-				    pid);
+			if (parent == 1) {
+				if (*cmdline) {
+					print_runtime_error(
+					    "extrace: process vanished before we found its parent: pid %d: %s\n",
+					    pid, cmdline);
+				} else {
+					print_runtime_error(
+					    "extrace: process vanished without a name: pid %d\n",
+					    pid);
+				}
 			}
 			close(proc_dir_fd);
 			return;